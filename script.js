6f7377c9d1e1a37a41231c581c0b1357750f1648
Severalbuttons88 <44512019+severalbuttons88@users.noreply.github.com>
2023-01-02 20:08:18 -0600

    Fix clear button bug

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-let gridSizeStorage = 16;
 const container = document.querySelector("#container");
+
 const gridBtn = document.querySelector("#grid-Button");
 const clearBtn = document.querySelector("#clear-Grid-Button");
 const flexContainer = document.querySelector("#flex-container");
+
+const sizeStorageDiv = document.createElement("div"); //stores size of div in class later
+sizeStorageDiv.setAttribute("id", "size-storage");
+container.appendChild(sizeStorageDiv);
 const flexContainerStyle = window.getComputedStyle(flexContainer);
 const flexContainerWidth = parseInt(
-
   flexContainerStyle.getPropertyValue("width")
 );
 const flexContainerHeight = parseInt(
@@ -34,59 +37,53 @@ function createGrid(numberOfGrids) {
 
     container.appendChild(gridArray[i]);
   }
-
 }
 function updateOnHover(gridToUpdate) {
-    gridToUpdate.addEventListener('mouseover', function(e) {
-        e.target.classList.remove("grid-default-color");
-        e.target.classList.add("grid-hover");                  //add hover class to grids
-
-    });
+  gridToUpdate.addEventListener("mouseover", function (e) {
+    e.target.classList.remove("grid-default-color");
+    e.target.classList.add("grid-hover"); //add hover class to grids
+  });
 }
 function gridUserUpdate() {
-    let userInput = prompt("Enter the size of the notepad you want: ", "16");
-    userInput = Number(userInput);
-    
+  let userInput = prompt("Enter the size of the notepad you want: ", "16");
+  userInput = Number(userInput);
+  if (userInput >= 100) {
+    userInput = 100;
+  } else if (userInput <= 4) {
+    userInput = 4;
+  } else if (typeof userInput != "number") {
+    userInput = prompt("Invalid input, please enter a number, ex (16): ");
+    parseInt(userInput);
+  }
+  sizeStorageDiv.removeAttribute("class");
 
-    console.log(userInput);
-    console.log(typeof userInput);
-    if (userInput >= 100) {
-        userInput = 100;
-    } else if (userInput <= 4) {
-        userInput = 4;
-    } else if (typeof userInput != "number") {
-        userInput = prompt("Invalid input, please enter a number, ex (16): ");
-        parseInt(userInput);
-    }
-    gridSizeStorage = userInput;
-
-    createGrid(userInput);
+  sizeStorageDiv.classList.add(`${userInput}`); //adds the users input as class to manipulate for clearing grid
+  console.log(sizeStorageDiv.getAttribute("class"));
+  createGrid(userInput);
 }
 function changeGridSizeButton() {
-    gridBtn.addEventListener("click", () => {
-        clearGrid();
-        gridUserUpdate();
-    });
+  gridBtn.addEventListener("click", () => {
+    clearGrid();
+    gridUserUpdate();
+  });
 }
-function gridUserClear(gridCurrentSize) {
-    gridCurrentSize = gridSizeStorage;
-    clearBtn.addEventListener("click", () => {
-        clearGrid();
-        createGrid(gridCurrentSize);
-        console.log(gridCurrentSize);
-    });
+
+function gridUserClear() {
+  clearBtn.addEventListener("click", () => {
+    let gridSizeStored = sizeStorageDiv.getAttribute("class")
+    gridSizeStored = Number(gridSizeStored);
+    console.log(gridSizeStored);
+   // sizeStorageDiv.removeAttribute("class");
+    clearGrid();
+    createGrid(gridSizeStored);
+  });
 }
 function startupPage() {
-    gridUserUpdate();
-    gridUserClear();
-    changeGridSizeButton();
-    gridUserClear(gridSizeStorage);
-
+  gridUserClear();
+  gridUserUpdate();
+  changeGridSizeButton();
 }
 function clearGrid() {
-    container.textContent = "";
-
+  container.textContent = "";
 }
 startupPage();
-
-
